b7e51e220334821cc249acff5a8d3bfeb881656b
ni326 <bigboy181101@gmail.com>
2023-08-19 23:59:13 +0900

    Twenty Commit

--- 104.py
+++ 104.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        print(root)
         if not root:
             return 0
-            
-        queue = [(root, 1)]
+        queue = [(root,1)] # root 節點位於深度為 1 的位置
         max_depth = 0
-        
         while queue:
-            current, depth = queue.pop(0)  
+            current, depth = queue.pop(0) # (currentNode, depth)
             max_depth = max(max_depth, depth)
             
             if current.left:
                 queue.append((current.left, depth + 1))
             if current.right:
                 queue.append((current.right, depth + 1))
-
         return max_depth
