1a8ee32cef88e92144a9ce9353e6734bc064eac4
ni326 <bigboy181101@gmail.com>
2023-09-09 23:59:46 +0900

    Fort

---
+++ 155.py
@@ -0,0 +1,24 @@
+class MinStack:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.stack = []
+        self.min_stack = []  # This stack will keep track of the minimum values
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            if self.stack[-1] == self.min_stack[-1]:
+                self.min_stack.pop()
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1] if self.stack else None
+
+    def getMin(self) -> int:
+        return self.min_stack[-1] if self.min_stack else None
