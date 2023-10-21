d651c3174d7e7918d85ea23fd7184a16e783a949
ni326 <bigboy181101@gmail.com>
2023-10-21 23:53:26 +0900

    Eighty one

---
+++ 92.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        prev = dummy
+        
+        # Move 'prev' to 'left-1' position
+        for _ in range(left - 1):
+            prev = prev.next
+        
+        # Reverse the sublist
+        current = prev.next
+        for _ in range(right - left):
+            next_node = current.next
+            current.next = next_node.next
+            next_node.next = prev.next
+            prev.next = next_node
+
+        return dummy.next
