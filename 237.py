60abaf0a3c7f1d9d6fea29cf39fe43539b61552e
ni326 <bigboy181101@gmail.com>
2023-10-27 23:57:16 +0900

    Eighty seven

--- 237.py
+++ 237.py
@@ -10,6 +10,12 @@ class Solution:
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
+        # node不應該是清單的最後一個節點，而應該是清單中的實際節點
+        # node 存在(不是 None)， node 有一個下一節點( next 不是 None)
         if node and node.next:
+            # node的值(val)設定為其下一個節點(node.next)的值
+            # 當前節點的值替換為其下一個節點的值    
             node.val = node.next.val
+            # 將當前節點node的下一個節點next設定為其下一個節點的下一個節點
+            # 前節點跳過其直接的下一個節點，直接連接到下下個節點
             node.next = node.next.next
