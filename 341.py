f3c71038c63d7ca5dba44afdd8a6efd32d294096
ni326 <bigboy181101@gmail.com>
2023-10-28 23:37:31 +0900

    Eighty eight

--- 341.py
+++ 341.py
@@ -1,20 +1,43 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def isInteger(self) -> bool:
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        """
+#
+#    def getInteger(self) -> int:
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        """
+#
+#    def getList(self) -> [NestedInteger]:
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        """
+
 class NestedIterator:
     def __init__(self, nestedList: [NestedInteger]):
-        self.stack = nestedList[::-1]  # reverse the list and use it as a stack
+        self.stack = nestedList[::-1]  # # 逆序放入堆疊，以保持正確的遍歷順序
 
     def next(self) -> int:
-        # The hasNext method ensures that the top of stack is an Integer
-        # so we can safely pop it and return the Integer.
         return self.stack.pop().getInteger()
 
     def hasNext(self) -> bool:
         while self.stack:
-            top = self.stack[-1]  # peek the top element
-            if top.isInteger():
+            top = self.stack[-1]
+            if top.isInteger(): #如果stack頂部的元素是一個整數（不是巢狀列表），直接返回True
                 return True
-            
-            # If the top element is a list, we will pop it and push its elements onto the stack.
-            self.stack.pop()
-            self.stack.extend(top.getList()[::-1])  # reverse the list before adding to stack
-        
-        return False
+            self.stack.pop() #如果是巢狀列表 需要將它移除以檢查其內部的元素
+            self.stack.extend(top.getList()[::-1])  # 取得stack頂部元素中的所有子元素，然後將它們逆序加入stack      
+        return False #如果迴圈結束而沒有返回True，那麼這意味著巢狀列表中沒有更多的整數，所以方法返回False
+         
+
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
