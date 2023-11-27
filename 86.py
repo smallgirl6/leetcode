55fda9a11cc216aeb97c8169a4f458a29adea806
ni326 <bigboy181101@gmail.com>
2023-11-27 23:54:15 +0900

    One hundred Twelve

--- 86.py
+++ 86.py
@@ -1,11 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        # Initialize two dummy nodes and their corresponding pointers.
         before_dummy = ListNode(0)
         after_dummy = ListNode(0)
         before, after = before_dummy, after_dummy
-        
-        # Iterate through the list
+
         while head:
             if head.val < x:
                 before.next = head
@@ -14,10 +17,8 @@ class Solution:
                 after.next = head
                 after = after.next
             head = head.next
-        
-        # Connect the before list to the after list.
+
         before.next = after_dummy.next
-        # Set the end of after list to None to prevent cycle in linked list
         after.next = None
         
         return before_dummy.next
