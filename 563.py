a4ef25c4b60fb8c96d8a4ea2741bd973f610bf98
ni326 <bigboy181101@gmail.com>
2023-12-05 23:56:14 +0900

    One Hundred Eighteen

--- 563.py
+++ 563.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def findTilt(self, root: Optional[TreeNode]) -> int:
         if not root:
@@ -19,11 +13,10 @@ class Solution:
                 right_sum = sum_stack.pop() if node.right else 0
                 total_tilt += abs(left_sum - right_sum)
                 sum_stack.append(node.val + left_sum + right_sum)
-            else: # not visted
+            else: #未訪問
                 stack.append((node,True))
                 if node.left:
                     stack.append((node.left, False))
                 if node.right:
                     stack.append((node.right, False))    
         return total_tilt
- 
