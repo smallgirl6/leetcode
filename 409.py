c1fa39f1e217402b507a2165f58e91aba620aecc
ni326 <bigboy181101@gmail.com>
2023-12-09 23:57:13 +0900

    Onehunded Twenty two

--- 409.py
+++ 409.py
@@ -1,7 +1,7 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
         counter = collections.Counter(s)
-        ### 回文字串時，通常每個字元都應該成對出現。 
+        ###### 回文字串時，通常每個字元都應該成對出現。 
         # abba 中，'a' 和 'b' 都是成對出現的。
         # 但可以允許有一個字符出現奇數次，比如 "abcba"，的 'c' 就是只出現一次的字符，它位於回文串的中心。
         # 如果字串中至少有一個字元出現了奇數次，has_odd 就會被設為 True，在建構回文字串時，使用這個奇數次出現的字元作為中心。
