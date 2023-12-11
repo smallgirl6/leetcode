624acf2e13e8484a6d304910f411532f7d2d8b31
ni326 <bigboy181101@gmail.com>
2023-12-11 23:04:32 +0900

    OneHundred Twenty four

--- 382.py
+++ 382.py
@@ -10,13 +10,14 @@ class Solution:
 
     def getRandom(self) -> int:
         current = self.head
-        chosen_value = None
-        n = 0
+        chosen_value = None # 儲存被選中節點的值
+        n = 0               # 遍歷過的節點
 
         while current:
             n += 1
-            if random.randint(1, n) == 1:
-                chosen_value = current.val
+            if random.randint(1, n) == 1: # 生成一個從 1 到 n 的隨機數 如果這個隨機數等於 1
+            # 遍歷每個節點時，生成的隨機數有 1/n 的機會等於 1 保證了所有節點被選中的機率是公平
+                chosen_value = current.val # 將當前節點的值賦給 chosen_value
             current = current.next
 
         return chosen_value
